a3ebebc7a86e7399d5bb6c28a475563bc639d9c5
gomeze23 <131938296+gomeze23@users.noreply.github.com>
2025-06-21 10:53:31 +1200

    Clarify variable names and explanations

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -6,19 +6,16 @@
  * GPS Sensor to identify the robot's position and heading on the field.
  */
 
-
 #ifndef DRIVETRAIN_HPP
 #define DRIVETRAIN_HPP
-
 
 /**
  * Controls the drivetrain motors using voltage. Used for driver control.
  * 
- * @param voltage_left The voltage of each motor on the left side of the drivetrain.
- * @param voltage_right The voltage of each motor on the right side of the drivetrain.
- * @param deadzone The minimum voltage required to spin the motors.
+ * @param left_voltage The voltage of each motor on the left side of the drivetrain.
+ * @param right_voltage The voltage of each motor on the right side of the drivetrain.
+ * @param min_voltage The minimum voltage required to spin the motors.
  */
-void drivetrain_driver_control(int voltage_left, int voltage_right, double deadzone);
-
+void drivetrain_driver_control(int left_voltage, int right_voltage, int min_voltage);
 
 #endif
--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -7,48 +7,50 @@
  * GPS Sensor to identify the robot's position and heading on the field.
  */
 
-
 #include "main.h"
 #include "drivetrain.hpp"
+#include <cmath>
 
+// The maximum voltage each motor can take.
+#define MAX_DT_VOLTAGE 127
 
-void drivetrain_driver_control(int voltage_left, int voltage_right, double deadzone)
+void drivetrain_driver_control(int left_voltage, int right_voltage, int min_voltage)
 {
-    // Left side motors are controlled with the left joystick.
-    // Only spin the motors when the left stick is past the deadzone.
-    if (voltage_left < (deadzone * -1) or voltage_left > deadzone)
+    // Spin the left side motors when the size of the left side's voltage is
+    // greater than the minimum required voltage.
+    if (abs(left_voltage) > min_voltage)
     {
         /**
-         * The range of the voltage was originally d <= V <= 127; the minimum is the
-         * deadzone (d) and the maximum is 127 V. This means that once the joystick
-         * goes past the deadzone, the drivetrain immediately has speed. To make it
-         * smoother, the range is changed to 0 <= V <= 127 so the robot starts at 0 V
-         * when the joystick goes past the deadzone. The calculation for the equation
-         * is below:
+         * The range of the voltage is initially m <= V <= 127. This means that between
+         * 0 V to m, the voltage is set to 0 V, and once the voltage goes over the
+         * minimum, the voltage immediately jumps up to m. To make it smoother, the
+         * range is changed to 0 <= V <= 127 so the robot starts at 0 V when the
+         * voltage goes over the minimum. The calculation for the equation is below:
          * 
-         * d <= V <= 127 (original)
-         * 0 <= V - d <= 127 - d (subtract d)
-         * 0 <= (V - d) / (127 - d) <= 1 (divide by 127 - d)
-         * 0 <= 127 * (V - d) / (127 - d) <= 127 (multiply by 127)
+         * m <= V <= 127
+         * 0 <= V - m <= 127 - d                  (subtract d)
+         * 0 <= (V - m) / (127 - d) <= 1          (divide by 127.0 - d)
+         * 0 <= 127 * (V - m) / (127 - d) <= 127  (multiply by 127.0)
          */
-        drivetrain_left.move((voltage_left - deadzone) * 127 / (127 - deadzone));
+        drivetrain_left.move((left_voltage - min_voltage) * MAX_DT_VOLTAGE / 
+            (MAX_DT_VOLTAGE - min_voltage));
     }
     else
     {
-        // Stop the motors when the joystick is inside the deadzone.
+        // Stop the motors when the input voltage is below the minimum.
         drivetrain_left.brake();
     }
 
-    // Right side motors are controlled with the right joystick.
-    // Only spin the motors when the right stick is past the deadzone.
-    if (voltage_right < (deadzone * -1) or voltage_right > deadzone)
+    // Spin the right side motors when the size of the right side's voltage is
+    // greater than the minimum required voltage.
+    if (abs(right_voltage) > min_voltage)
     {
-        // (read explanation above)
-        drivetrain_right.move((voltage_right - deadzone) * 100 / (100 - deadzone));
+        drivetrain_right.move((right_voltage - min_voltage) * MAX_DT_VOLTAGE / 
+            (MAX_DT_VOLTAGE - min_voltage));
     }
     else
     {
-        // Stop the motors when the joystick is inside the deadzone.
+        // Stop the motors when the input voltage is below the minimum.
         drivetrain_right.brake();
     }
 }
