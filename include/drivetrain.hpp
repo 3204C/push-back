96e189319b150996842786f3d0308e6d27ef8165
gomeze23 <131938296+gomeze23@users.noreply.github.com>
2025-08-16 10:28:04 +1200

    Add autonomous driving functions

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -16,6 +16,33 @@
  * @param right_voltage The voltage of each motor on the right side of the drivetrain.
  * @param min_voltage The minimum voltage required to spin the motors.
  */
-void drivetrain_driver_control(int left_voltage, int right_voltage, int min_voltage);
+void dt_move(int left_voltage, int right_voltage, int min_voltage);
+
+/**
+ * Controls the drivetrain motors using voltage for an amount of time.
+ * Used for autonomous.
+ * 
+ * @param left_voltage The voltage of each motor on the left side of the drivetrain.
+ * @param right_voltage The voltage of each motor on the right side of the drivetrain.
+ * @param min_voltage The minimum voltage required to spin the motors.
+ * @param duration The amount of time (in ms) the drivetrain motors spin for.
+ */
+void dt_move(int left_voltage, int right_voltage, int min_voltage, int duration);
+
+/**
+ * Moves the drivetrain in a straight line for a given distance and duration of time.
+ * 
+ * @param distance The length of displacement from the robot's original position (in inches).
+ * @param duration The amount of time (in ms) the drivetrain motors spin for.
+ */
+void dt_move_straight(double distance, int duration);
+
+/**
+ * Turns the drivetrain at a given angle and for a duration of time.
+ * 
+ * @param angle The angular displacement from the robot's original orientation (in degrees).
+ * @param duration The amount of time (in ms) the drivetrain motors spin for.
+ */
+void dt_turn(double angle, int duration);
 
 #endif
--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -11,26 +11,29 @@
 #include "drivetrain.hpp"
 #include <cmath>
 
-// The maximum voltage each motor can take.
+/// The maximum voltage each motor can take.
 #define MAX_DT_VOLTAGE 127
 
-void drivetrain_driver_control(int left_voltage, int right_voltage, int min_voltage)
+/// The radius of each drivetrain wheel (in inches).
+#define WHEEL_RADIUS 2
+
+/// The distance between the two wheels on each side.
+#define TRACK_WIDTH 15
+
+void dt_move(int left_voltage, int right_voltage, int min_voltage)
 {
     // Spin the left side motors when the size of the left side's voltage is
     // greater than the minimum required voltage.
     if (abs(left_voltage) > min_voltage)
     {
         /**
-         * The range of the voltage is initially m <= V <= 127. This means that between
-         * 0 V to m, the voltage is set to 0 V, and once the voltage goes over the
-         * minimum, the voltage immediately jumps up to m. To make it smoother, the
-         * range is changed to 0 <= V <= 127 so the robot starts at 0 V when the
-         * voltage goes over the minimum. The calculation for the equation is below:
+         * The range of the input voltage is changed from min <= V <= 127 to
+         * 0 <= 127 * (V - min) / (127 - min) <= 127 using the calculations below:
          * 
-         * m <= V <= 127
-         * 0 <= V - m <= 127 - d                  (subtract d)
-         * 0 <= (V - m) / (127 - d) <= 1          (divide by 127.0 - d)
-         * 0 <= 127 * (V - m) / (127 - d) <= 127  (multiply by 127.0)
+         * 4 <= V <= 127
+         * 0 <= V - min <= 127 - min                    (subtract d)
+         * 0 <= (V - min) / (127 - min) <= 1            (divide by 127 - 4)
+         * 0 <= 127 * (V - min) / (127 - min) <= 127    (multiply by 127)
          */
         drivetrain_left.move((left_voltage - min_voltage) * MAX_DT_VOLTAGE / 
             (MAX_DT_VOLTAGE - min_voltage));
@@ -53,4 +56,71 @@ void drivetrain_driver_control(int left_voltage, int right_voltage, int min_volt
         // Stop the motors when the input voltage is below the minimum.
         drivetrain_right.brake();
     }
+}
+
+void dt_move(int left_voltage, int right_voltage, int min_voltage, int duration)
+{
+    // Spin the motors with the specified values.
+    dt_move(left_voltage, right_voltage, min_voltage);
+    
+    // The drivetrain will stop after the set duration has passed.
+    pros::delay(duration);
+    drivetrain_left.brake();
+    drivetrain_right.brake();
+}
+
+void dt_move_straight(double distance, int duration)
+{
+    /**
+     * Calculate the angular velocity of each motor using the formulas below:
+     * 
+     * v (in/min) = d (in) / t (ms) * 60000 (ms/min)    (velocity of wheel)
+     * v (in/min) = r (in) * omega (rot/min)
+     * omega (rot/min) = v (in/min) / r (in)            (angular velocity of motor)
+     */
+    double velocity = distance / duration * 60000;
+    double angular_velocity = velocity / WHEEL_RADIUS;
+
+    // Spin the motors with the calculated angular velocity.
+    drivetrain_left.move_velocity(angular_velocity);
+    drivetrain_right.move_velocity(angular_velocity);
+
+    // The drivetrain will stop after the set duration has passed.
+    pros::delay(duration);
+    drivetrain_left.brake();
+    drivetrain_right.brake();
+}
+
+void dt_turn(double angle, int duration)
+{
+    /**
+     * Calculate the angular velocity of each motor using the formulas below:
+     * 
+     * R (in) = 1/2 * sqrt([wheelbase]^2 + [track width]^2) (radius of turning circle)
+     * d (in) = R (in) * THETA (deg) / 360 (deg)            (displacement of wheel)
+     * v (in/min) = d (in) / t (ms) * 60000 (ms/min)        (velocity of wheel)
+     * v (in/min) = r (in) * omega (rot/min)                
+     * omega (rot/min) = v (in/min) / r (in)                (angular velocity of motor)
+     */
+    double turning_radius = TRACK_WIDTH / 2;
+    double distance = turning_radius * abs(angle) / 360;
+    double velocity = distance / duration * 60000;
+    double angular_velocity = velocity / WHEEL_RADIUS;
+
+    // Determine the direction of rotation, then spin the motors as desired.
+    if (angle > 0)
+    {
+        drivetrain_left.move_velocity(angular_velocity);
+        drivetrain_right.move_velocity(-angular_velocity);
+    }
+    else if (angle < 0)
+    {
+        drivetrain_left.move_velocity(-angular_velocity);
+        drivetrain_right.move_velocity(angular_velocity);
+    }
+    
+    // The drivetrain will stop after the set duration has passed.
+    pros::delay(duration);
+    drivetrain_left.brake();
+    drivetrain_right.brake();
 }
