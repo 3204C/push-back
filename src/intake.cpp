4218745a43e8c435467464e9389ddb16b15e43f8
gomeze23 <131938296+gomeze23@users.noreply.github.com>
2025-08-16 10:29:54 +1200

    Refactor intake direction to use absolute value

--- src/intake.cpp
+++ src/intake.cpp
@@ -17,47 +17,23 @@ void intake_spin(int is_intake_pressed, int is_outtake_pressed)
     // Calculate the net direction.
     int direction = is_intake_pressed - is_outtake_pressed;
 
-    // The intake spins inwards if the direction is positive.
-    if (direction > 0)
-    {
-        intake.move(INTAKE_VOLTAGE);
-    }
-
-    // The intake spins outwards if the direction is negative.
-    else if (direction < 0)
-    {
-        intake.move(-INTAKE_VOLTAGE);
-    }
-
     // If no direction is given, nothing happens.
-    else
+    if (direction == 0)
     {
         intake.brake();
+    }
+
+    // The intake spins inwards if the direction is positive and spins outwards
+    // if the direction is negative.
+    else
+    {
+        intake.move(INTAKE_VOLTAGE * direction);
     }
 }
 
 void intake_spin(int is_intake_pressed, int is_outtake_pressed, int duration)
 {   
-    // Calculate the net direction.
-    int direction = is_intake_pressed - is_outtake_pressed;
-
-    // The intake rotates inwards if the direction is positive.
-    if (direction > 0)
-    {
-        intake.move(INTAKE_VOLTAGE);
-    }
-
-    // The intake rotates outwards if the direction is negative.
-    else if (direction < 0)
-    {
-        intake.move(-INTAKE_VOLTAGE);
-    }
-
-    // If no direction is given, nothing happens.
-    else
-    {
-        intake.brake();
-    }
+    intake_spin(is_intake_pressed, is_outtake_pressed);
 
     // The intake will stop after the set duration has passed.
     pros::delay(duration);
